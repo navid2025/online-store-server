8157d36d7e0e198bd30757a38d29cc1bc3f1214f
n.zare <n.zare@behsacorp.com>
2024-02-07 16:38:55 +0330

    update customer controller with service. closes #5

--- src/main/java/com/navid/onlinestoreserver/web/controller/CustomerController.java
+++ src/main/java/com/navid/onlinestoreserver/web/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.navid.onlinestoreserver.web.controller;
 
 
+import com.navid.onlinestoreserver.domain.service.CustomerService;
 import com.navid.onlinestoreserver.web.model.CustomerDto;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,29 +11,35 @@ import java.util.List;
 @RequestMapping("/customers")
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping
     public List<CustomerDto> getAllCustomer(){
-        return null;
+        return customerService.findAllCustomers();
     }
 
     @GetMapping("/{id}")
     public CustomerDto getCustomerById(@PathVariable Long id){
-        return null;
+        return customerService.findById(id);
     }
 
     @PostMapping
     public CustomerDto createCustomer(@RequestBody CustomerDto customerDto){
-        return null;
+        return customerService.save(customerDto);
     }
 
     @PutMapping
     public CustomerDto updateCustomer(@RequestBody CustomerDto customerDto){
-        return null;
+        return customerService.save(customerDto);
     }
 
     @DeleteMapping("/{id}")
     public void deleteCustomer(@RequestBody Long id){
-        return;
+        customerService.deleteById(id);
     }
 
 
